519550d31ac53047895833270f6cf3da60573f12
SUSIKING <iker.susaeta@gmail.com>
2026-06-17 11:54:22 -0400

    Bump version to v0.0.1.0

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-export const APP_VERSION = 'v0.0.0.9';
-export const BUILD_TIMESTAMP_CL = '2026-06-17 08:18 CLT';
+export const APP_VERSION = 'v0.0.1.0';
+export const BUILD_TIMESTAMP_CL = '2026-06-17 08:31 CLT';
 
 export const WALL = 0;
 export const PATH = 1;
