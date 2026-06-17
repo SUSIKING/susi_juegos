bffee3f6285bbf19e72200a5866fffe31d2efe53
SUSIKING <iker.susaeta@gmail.com>
2026-06-17 11:55:39 -0400

    Update maze cache key to v010

--- js/maze.js
+++ js/maze.js
@@ -1,4 +1,4 @@
-import { WALL, PATH, levelParams } from './config.js?v=009';
+import { WALL, PATH, levelParams } from './config.js?v=010';
 
 export function buildMaze(level){
   const params = levelParams(level);
